961c4d194a6d26ab66f0e2cc60ef5ff0c8dfd314
idaki <idaki@me.com>
2023-12-10 13:03:24 +0100

    lab dom events completed

--- JS Front-End/07.JS-Front-End-DOM-Events/Lab/01. List-Of-Items/app.js
+++ JS Front-End/07.JS-Front-End-DOM-Events/Lab/01. List-Of-Items/app.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+    const targetElement = document.getElementById('items');
+    const text = document.querySelector('input[type="text"]').value;
+                
+    
+    const newElement = document.createElement('li');
+  
+    newElement.innerHTML = text;
+
+    targetElement.appendChild(newElement);
+
 }
