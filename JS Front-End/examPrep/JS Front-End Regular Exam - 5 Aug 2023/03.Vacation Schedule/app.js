86ddf94abf5fdc4517a736a8906d697e9d3f8c72
idaki <idaki@me.com>
2023-12-16 11:21:53 +0100

    vacation exam

--- JS Front-End/examPrep/JS Front-End Regular Exam - 5 Aug 2023/03.Vacation Schedule/app.js
+++ JS Front-End/examPrep/JS Front-End Regular Exam - 5 Aug 2023/03.Vacation Schedule/app.js
@@ -1,32 +1,151 @@
-document.getElementById('add-vacation').addEventListener('click', function(event){
-    event.preventDefault();
 
-    // Get values from the form
-    var name = document.getElementById('name').value;
-    var numDays = document.getElementById('num-days').value;
-    var fromDate = document.getElementById('from-date').value;
+const baseURL = 'http://localhost:3030/jsonstore/tasks/';
 
-    // Create vacation object
-    var vacation = {
-        name: name,
-        numDays: numDays,
-        fromDate: fromDate
+const loadBtn = document.getElementById('load-vacations');
+const vacationList = document.getElementById('list');
+const formElement = document.querySelector('#form form');
+const nameInput = document.getElementById('name');
+const durationInput = document.getElementById('num-days');
+const formDateInput = document.getElementById('from-date');
+const formAddBtn = document.getElementById('add-vacation');
+const formEditBtn = document.getElementById('edit-vacation');
+
+
+loadBtn.addEventListener('click', loadVacations);
+
+formAddBtn.addEventListener('click', (e) => {
+    const newVacation = {
+        name: nameInput.value,
+        days: durationInput.value,
+        date: formDateInput.value
     };
 
-    // Send POST request to server
-    fetch('/addVacation', {
+    fetch(baseURL, {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
+        headers: { // Corrected the typo here
+            'Content-Type': 'application/json'
         },
-        body: JSON.stringify(vacation),
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-        // Here you can add code to update the UI
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
+        body: JSON.stringify(newVacation)
+    }).then(loadVacations)
+        .then(clearForm());
+
 });
+
+formEditBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const vacationId = formEditBtn.dataset.vacationId; // Access vacation ID from formEditBtnugger;
+    const vacationData = {
+        _id: vacationId,
+        name: nameInput.value,
+        days: durationInput.value,
+        date: formDateInput.value
+    };
+    console.log(vacationId);
+
+    fetch(`${baseURL}${vacationId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(vacationData)
+    })
+        .then(loadVacations)
+        .then(() => {
+
+            clearForm();
+            formAddBtn.removeAttribute('disabled');
+            formEditBtn.setAttribute('disabled', 'disabled');
+            delete formEditBtn.dataset.vacationId; // Remove the vacation ID from the dataset
+        })
+
+
+
+
+})
+
+function clearForm() {
+    nameInput.value = '';
+    durationInput.value = ''; // Use durationInput instead of numDaysInput
+    formDateInput.value = '';
+};
+
+function renderVacations(vacations) {
+    vacationList.innerHTML = '';
+    vacations.map(vacation => renderVacation(vacation))
+        .forEach(vacationElement => vacationList.appendChild(vacationElement));
+}
+
+
+function renderVacation(vacation) {
+
+
+    const container = document.createElement('div');
+    container.className = ('container');
+
+    const h2userName = document.createElement('h2');
+    h2userName.textContent = vacation.name;
+
+    const h3date = document.createElement('h3');
+    h3date.textContent = vacation.date;
+
+    const h3duration = document.createElement('h3');
+    h3duration.textContent = vacation.days;
+
+    const changeBtn = document.createElement('button');
+    changeBtn.className = 'change-btn';
+    changeBtn.textContent = 'Change';
+    changeBtn.addEventListener('click', () => {
+
+
+        nameInput.value = vacation.name;
+        durationInput.value = vacation.days; // Corrected variable name
+        formDateInput.value = vacation.date;
+
+        container.remove();
+        formAddBtn.setAttribute('disabled', 'disabled');
+        formEditBtn.removeAttribute('disabled');
+
+
+        formEditBtn.dataset.vacationId = vacation._id;
+
+    });
+
+    const doneBtn = document.createElement('button');
+    doneBtn.className = 'done-btn';
+    doneBtn.textContent = 'Done';
+
+    doneBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+    
+        fetch(`${baseURL}${vacation._id}`, { // Corrected to use backticks and template literals
+            method: 'DELETE'
+        }).then(loadVacations);
+    });
+
+    container.appendChild(h2userName);
+    container.appendChild(h3date);
+    container.appendChild(h3duration);
+    container.appendChild(changeBtn);
+    container.appendChild(doneBtn);
+
+    return container;
+
+}
+
+function loadVacations() {
+
+    return fetch(baseURL)
+        .then(res => res.json())
+        .then(result => {
+            const vacations = Object.values(result); // Convert result to an array of values
+            renderVacations(vacations); // Pass this array to renderVacations
+        });
+}
+
+
+
+
+
+
+
+
--- JS Front-End/examPrep/JS Front-end Exam Preparation-2-Resources/03.Weather Tracker/app.js
+++ JS Front-End/examPrep/JS Front-end Exam Preparation-2-Resources/03.Weather Tracker/app.js
@@ -1,3 +1,135 @@
+// // function solve() {
+// //     const baseURL = 'http://localhost:3030/jsonstore/tasks/';
+
+// //     const createElement = (tag, content = '', classes = [], id = '', listener = '') => {
+// //         const element = document.createElement(tag);
+
+// //         if (content) {
+// //             element.textContent = content; // Fixed from 'content' to 'textContent'
+// //         }
+
+// //         if (classes.length) {
+// //             element.classList.add(...classes);
+// //         }
+
+// //         if (id) {
+// //             element.id = id;
+// //         }
+
+// //         if (listener) {
+// //             element.addEventListener('click', listener); // Fixed from 'addEventLister' to 'addEventListener'
+// //         }
+// //         return element;
+// //     }
+
+// //     const buttonHandler = (e) => { // Corrected spelling from 'buttenHandler' to 'buttonHandler'
+// //         const operations = {
+// //             "Add Weather": addWeather,
+// //             "Edit Weather": editWeather, // Fixed spelling from 'Edi tWeather' to 'Edit Weather'
+// //             "Load History": loadHistory,
+// //             "Change Weather": changeWeather,
+// //             "Delete Weather": deleteWeather,
+// //         };
+
+// //         operations[e.target.textContent](e); // Fixed from 'operetions' to 'operations'
+// //     }
+
+// //     const changeWeather = (e) => {
+// //         const mainDiv = e.target.parentElement.parentElement;
+// //         editID = mainDiv.id;
+
+// //         const location = mainDiv.querySelector('h2').textContent;
+// //         const [date, temp] = mainDiv.querySelector('h3'); // Fixed the destructuring
+
+// //         fields.location.value = location;
+// //         fields.date.value = date.textContent;
+// //         fields.temp.value = temp.textContent;
+
+// //         editWeatherBtn.disabled = false;
+// //         addWeatherBtn.disabled = true;
+// //         mainDiv.remove();
+// //     }
+
+// //     const createNewWeather = () => {
+// //         const newWeather = {};
+
+// //         for (const [key, value] of Object.entries(fields)) { // Fixed from 'Object.entries[fields]' to 'Object.entries(fields)'
+// //             newWeather[key] = value.value;
+// //         }
+// //         return newWeather; // Added return statement
+// //     }
+
+// //     const addWeather = (e)=>{
+// //         fetch(baseURL,{
+// //             method:'POST',
+// //             body:JSON.stringify(createNewWeather())
+// //         }).then(()=>{
+
+// //         })
+// //     }
+
+// //     const loadHistory =(e)=>{
+// //         fetch(baseURL)
+// //         .then(res => res.json())
+// //         .then(data=>{
+// //             listContainer.innerHTML='';
+// //             Object.values[data].forEach(element => {
+// //                 const containerDiv = createElement('div','',['container'], id = item._id);
+// //             containerDiv.appendChild(createElement('h2',item.location));
+// //             containerDiv.appendChild(createElement('h2',item.date));
+// //             containerDiv.appendChild(createElement('h2',item.temp,[],'celsius'));
+            
+// //             const btnContainer = createElement('div','',["buttons-contianer"]);           
+          
+// //             btnContainer.appendChild(createElement('button','Change',[''],'',buttonHandler));
+// //             btnContainer.appendChild(createElement('button','Delte',[''],'',buttonHandler));
+// //             containerDiv.appendChild(btnContainer);
+// //             listContainer.appendChild(containerDiv);
+// //         });
+// //         })
+// //     }
+
+// //     const editWeather = (e) => {
+// //         fetch(`${baseURL}${editID}`, {
+// //             method: "PUT",
+// //             body: JSON.stringify(createNewWeather())
+// //         })
+// //             .then(() => {
+// //                 editWeatherBtn.disabled = true;
+// //                 addWeatherBtn.disabled = false;
+// //             })
+// //     }
+
+// //     let editID = ''; // Initialized editID
+
+// //     const fields = {
+// //         location: document.getElementById('location'),
+// //         temp: document.getElementById('temp'),
+// //         date: document.getElementById('date'),
+// //     };
+
+// //     const loadbtn = document.getElementById('load-history');
+// //     loadbtn.addEventListener('click', buttonHandler); // Corrected to 'buttonHandler'
+
+// //     const addWeatherBtn = document.getElementById('add-weather');
+// //     addWeatherBtn.addEventListener('click', buttonHandler); // Corrected to 'buttonHandler'
+
+// //     const editWeatherBtn = document.getElementById('edit-weather');
+// //     editWeatherBtn.addEventListener('click', buttonHandler); // Corrected to 'buttonHandler'
+
+// //     const listContainer = document.getElementById('list');
+// // }
+
+// // const deleteWeather = (e)=>{
+// //     editID= e.target.parentElement.parentElement;
+// //     fetch(`${baseURL}${editID}`,{
+// //         method: DELETE,   
+// //     }).then(()=>{
+// // loadHistory()
+// //     })
+// // }
+
+// // solve();
 // function solve() {
 //     const baseURL = 'http://localhost:3030/jsonstore/tasks/';
 
@@ -5,7 +137,7 @@
 //         const element = document.createElement(tag);
 
 //         if (content) {
-//             element.textContent = content; // Fixed from 'content' to 'textContent'
+//             element.textContent = content;
 //         }
 
 //         if (classes.length) {
@@ -17,21 +149,21 @@
 //         }
 
 //         if (listener) {
-//             element.addEventListener('click', listener); // Fixed from 'addEventLister' to 'addEventListener'
+//             element.addEventListener('click', listener);
 //         }
 //         return element;
 //     }
 
-//     const buttonHandler = (e) => { // Corrected spelling from 'buttenHandler' to 'buttonHandler'
+//     const buttonHandler = (e) => {
 //         const operations = {
 //             "Add Weather": addWeather,
-//             "Edit Weather": editWeather, // Fixed spelling from 'Edi tWeather' to 'Edit Weather'
+//             "Edit Weather": editWeather,
 //             "Load History": loadHistory,
 //             "Change Weather": changeWeather,
 //             "Delete Weather": deleteWeather,
 //         };
 
-//         operations[e.target.textContent](e); // Fixed from 'operetions' to 'operations'
+//         operations[e.target.textContent](e);
 //     }
 
 //     const changeWeather = (e) => {
@@ -39,7 +171,7 @@
 //         editID = mainDiv.id;
 
 //         const location = mainDiv.querySelector('h2').textContent;
-//         const [date, temp] = mainDiv.querySelector('h3'); // Fixed the destructuring
+//         const [date, temp] = mainDiv.querySelectorAll('h3');
 
 //         fields.location.value = location;
 //         fields.date.value = date.textContent;
@@ -53,40 +185,39 @@
 //     const createNewWeather = () => {
 //         const newWeather = {};
 
-//         for (const [key, value] of Object.entries(fields)) { // Fixed from 'Object.entries[fields]' to 'Object.entries(fields)'
+//         for (const [key, value] of Object.entries(fields)) {
 //             newWeather[key] = value.value;
 //         }
-//         return newWeather; // Added return statement
+//         return newWeather;
 //     }
 
-//     const addWeather = (e)=>{
-//         fetch(baseURL,{
-//             method:'POST',
-//             body:JSON.stringify(createNewWeather())
-//         }).then(()=>{
-
-//         })
-//     }
-
-//     const loadHistory =(e)=>{
-//         fetch(baseURL)
-//         .then(res => res.json())
-//         .then(data=>{
-//             listContainer.innerHTML='';
-//             Object.values[data].forEach(element => {
-//                 const containerDiv = createElement('div','',['container'], id = item._id);
-//             containerDiv.appendChild(createElement('h2',item.location));
-//             containerDiv.appendChild(createElement('h2',item.date));
-//             containerDiv.appendChild(createElement('h2',item.temp,[],'celsius'));
-            
-//             const btnContainer = createElement('div','',["buttons-contianer"]);           
-          
-//             btnContainer.appendChild(createElement('button','Change',[''],'',buttonHandler));
-//             btnContainer.appendChild(createElement('button','Delte',[''],'',buttonHandler));
-//             containerDiv.appendChild(btnContainer);
-//             listContainer.appendChild(containerDiv);
+//     const addWeather = (e) => {
+//         fetch(baseURL, {
+//             method: 'POST',
+//             body: JSON.stringify(createNewWeather())
+//         }).then(() => {
+//             // Handle the response (e.g., reload the list)
 //         });
-//         })
+//     }
+
+//     const loadHistory = (e) => {
+//         fetch(baseURL)
+//             .then(res => res.json())
+//             .then(data => {
+//                 listContainer.innerHTML = '';
+//                 Object.values(data).forEach(item => {
+//                     const containerDiv = createElement('div', '', ['container'], item._id);
+//                     containerDiv.appendChild(createElement('h2', item.location));
+//                     containerDiv.appendChild(createElement('h3', item.date));
+//                     containerDiv.appendChild(createElement('h3', item.temp, [], 'celsius'));
+            
+//                     const btnContainer = createElement('div', '', ["buttons-container"]);           
+//                     btnContainer.appendChild(createElement('button', 'Change', [], '', buttonHandler));
+//                     btnContainer.appendChild(createElement('button', 'Delete', [], '', buttonHandler));
+//                     containerDiv.appendChild(btnContainer);
+//                     listContainer.appendChild(containerDiv);
+//                 });
+//             });
 //     }
 
 //     const editWeather = (e) => {
@@ -100,7 +231,16 @@
 //             })
 //     }
 
-//     let editID = ''; // Initialized editID
+//     const deleteWeather = (e) => {
+//         const deleteID = e.target.parentElement.parentElement.id;
+//         fetch(`${baseURL}${deleteID}`, {
+//             method: 'DELETE'
+//         }).then(() => {
+//             loadHistory();
+//         });
+//     }
+
+//     let editID = '';
 
 //     const fields = {
 //         location: document.getElementById('location'),
@@ -109,155 +249,51 @@
 //     };
 
 //     const loadbtn = document.getElementById('load-history');
-//     loadbtn.addEventListener('click', buttonHandler); // Corrected to 'buttonHandler'
+//     loadbtn.addEventListener('click', loadHistory);
 
 //     const addWeatherBtn = document.getElementById('add-weather');
-//     addWeatherBtn.addEventListener('click', buttonHandler); // Corrected to 'buttonHandler'
+//     addWeatherBtn.addEventListener('click', addWeather);
 
 //     const editWeatherBtn = document.getElementById('edit-weather');
-//     editWeatherBtn.addEventListener('click', buttonHandler); // Corrected to 'buttonHandler'
+//     editWeatherBtn.addEventListener('click', editWeather);
 
 //     const listContainer = document.getElementById('list');
 // }
 
-// const deleteWeather = (e)=>{
-//     editID= e.target.parentElement.parentElement;
-//     fetch(`${baseURL}${editID}`,{
-//         method: DELETE,   
-//     }).then(()=>{
-// loadHistory()
-//     })
-// }
-
 // solve();
-function solve() {
-    const baseURL = 'http://localhost:3030/jsonstore/tasks/';
 
-    const createElement = (tag, content = '', classes = [], id = '', listener = '') => {
-        const element = document.createElement(tag);
+function solve(){
 
-        if (content) {
-            element.textContent = content;
-        }
-
-        if (classes.length) {
-            element.classList.add(...classes);
-        }
-
-        if (id) {
-            element.id = id;
-        }
-
-        if (listener) {
-            element.addEventListener('click', listener);
-        }
-        return element;
-    }
-
-    const buttonHandler = (e) => {
-        const operations = {
-            "Add Weather": addWeather,
-            "Edit Weather": editWeather,
-            "Load History": loadHistory,
-            "Change Weather": changeWeather,
-            "Delete Weather": deleteWeather,
-        };
-
-        operations[e.target.textContent](e);
-    }
-
-    const changeWeather = (e) => {
-        const mainDiv = e.target.parentElement.parentElement;
-        editID = mainDiv.id;
-
-        const location = mainDiv.querySelector('h2').textContent;
-        const [date, temp] = mainDiv.querySelectorAll('h3');
-
-        fields.location.value = location;
-        fields.date.value = date.textContent;
-        fields.temp.value = temp.textContent;
-
-        editWeatherBtn.disabled = false;
-        addWeatherBtn.disabled = true;
-        mainDiv.remove();
-    }
-
-    const createNewWeather = () => {
-        const newWeather = {};
-
-        for (const [key, value] of Object.entries(fields)) {
-            newWeather[key] = value.value;
-        }
-        return newWeather;
-    }
-
-    const addWeather = (e) => {
-        fetch(baseURL, {
-            method: 'POST',
-            body: JSON.stringify(createNewWeather())
-        }).then(() => {
-            // Handle the response (e.g., reload the list)
-        });
-    }
-
-    const loadHistory = (e) => {
-        fetch(baseURL)
-            .then(res => res.json())
-            .then(data => {
-                listContainer.innerHTML = '';
-                Object.values(data).forEach(item => {
-                    const containerDiv = createElement('div', '', ['container'], item._id);
-                    containerDiv.appendChild(createElement('h2', item.location));
-                    containerDiv.appendChild(createElement('h3', item.date));
-                    containerDiv.appendChild(createElement('h3', item.temp, [], 'celsius'));
-            
-                    const btnContainer = createElement('div', '', ["buttons-container"]);           
-                    btnContainer.appendChild(createElement('button', 'Change', [], '', buttonHandler));
-                    btnContainer.appendChild(createElement('button', 'Delete', [], '', buttonHandler));
-                    containerDiv.appendChild(btnContainer);
-                    listContainer.appendChild(containerDiv);
-                });
-            });
-    }
-
-    const editWeather = (e) => {
-        fetch(`${baseURL}${editID}`, {
-            method: "PUT",
-            body: JSON.stringify(createNewWeather())
+document.getElementById('load-history').addEventListener('click', function() {
+    fetch('your-api-endpoint-here') // Replace with your API endpoint
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
         })
-            .then(() => {
-                editWeatherBtn.disabled = true;
-                addWeatherBtn.disabled = false;
-            })
-    }
-
-    const deleteWeather = (e) => {
-        const deleteID = e.target.parentElement.parentElement.id;
-        fetch(`${baseURL}${deleteID}`, {
-            method: 'DELETE'
-        }).then(() => {
-            loadHistory();
+        .then(data => {
+            const historyList = document.getElementById('list');
+            historyList.innerHTML = ''; // Clear existing list
+            data.forEach(record => {
+                const recordElement = document.createElement('div');
+                recordElement.className = 'container';
+                // Populate recordElement with record data
+                recordElement.innerHTML = `
+                    <h2>${record.location}</h2>
+                    <h3>${record.date}</h3>
+                    <h3 id="celsius">${record.temperature}</h3>
+                    <div id="buttons-container">
+                        <button class="change-btn">Change</button>
+                        <button class="delete-btn">Delete</button>
+                    </div>
+                `;
+                historyList.appendChild(recordElement);
+            });
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
         });
-    }
+});
 
-    let editID = '';
-
-    const fields = {
-        location: document.getElementById('location'),
-        temp: document.getElementById('temp'),
-        date: document.getElementById('date'),
-    };
-
-    const loadbtn = document.getElementById('load-history');
-    loadbtn.addEventListener('click', loadHistory);
-
-    const addWeatherBtn = document.getElementById('add-weather');
-    addWeatherBtn.addEventListener('click', addWeather);
-
-    const editWeatherBtn = document.getElementById('edit-weather');
-    editWeatherBtn.addEventListener('click', editWeather);
-
-    const listContainer = document.getElementById('list');
 }
-
-solve();
